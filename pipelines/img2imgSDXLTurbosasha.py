78c41029fe353a5e8ba19364563574406961f0c8
Sasha Dog <sashashengyt@gmail.com>
2023-12-09 23:24:18 +0000

    sasha

--- pipelines/img2imgSDXLTurbosasha.py
+++ pipelines/img2imgSDXLTurbosasha.py
@@ -15,38 +15,60 @@ from config import Args
 from pydantic import BaseModel, Field
 from PIL import Image
 import math
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 base_model = "stabilityai/sdxl-turbo"
 taesd_model = "madebyollin/taesdxl"
 
 default_prompt = "close-up photography of old man standing in the rain at night, in a street lit by lamps, leica 35mm summilux"
 default_negative_prompt = "blurry, low quality, render, 3D, oversaturated"
-page_content = """
-<h1 class="text-3xl font-bold">Real-Time SDXL Turbo</h1>
-<h3 class="text-xl font-bold">Image-to-Image</h3>
-<p class="text-sm">
-    This demo showcases
-    <a
-    href="https://huggingface.co/stabilityai/sdxl-turbo"
-    target="_blank"
-    class="text-blue-500 underline hover:no-underline">SDXL Turbo</a>
-Image to Image pipeline using
-    <a
-    href="https://huggingface.co/docs/diffusers/main/en/using-diffusers/sdxl_turbo"
-    target="_blank"
-    class="text-blue-500 underline hover:no-underline">Diffusers</a
-    > with a MJPEG stream server.
-</p>
-<p class="text-sm text-gray-500">
-    Change the prompt to generate different images, accepts <a
-    href="https://github.com/damian0815/compel/blob/main/doc/syntax.md"
-    target="_blank"
-    class="text-blue-500 underline hover:no-underline">Compel</a
-    > syntax.
-</p>
-"""
+page_content = """"""
+
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+if not client.api_key.startswith("sk-"):
+    # throw error 'Please enter your OpenAI API key!'
+    raise ValueError("Please enter your OpenAI API key!") 
 
 
+def pil_to_base64(image):
+    buffered = BytesIO()
+    image.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue())
+    return img_str.decode("utf-8")
+
+
+def get_sys_prompt(self, use_gptv):
+    what_users_will_say=[
+        ("make me look like an old man", "a close up image of an old man"),
+        ("more beautiful", "a close up image of a beautiful person") ,
+        ("make me stand below a street light", "a close up image of a person standing below a street light"),
+        ("I want to look like a dog", "a close up image of a person in a dog costume"),
+        ("A person in a cat costume", "a close up image of a person in a cat costume"),
+        ("Why not be an native indian", "a close up image of an native indian"),
+        ("So what this also shows is that you can say you want to be a cat, and it will generate a dog eating meat for you", "a close up image of a person impersonating a cat"),
+        ("It will generate a cat with your face", "a close up image of a person in a cat costume"),
+    ]
+    
+    things_to_capture = '\n'.join([t[0] for t in what_users_will_say])
+    correct_things = '\n'.join([t[1] for t in what_users_will_say])
+    prompt_for_GPT = f"Generate a Stable Diffusion prompt that captures things like {things_to_capture} into {correct_things}"
+    
+    prompt_for_GPTV = """
+    You are a GPT Vision bot. Take an image of a person and use the image description to generate a prompt for stable diffusion. For example change the prompt from:
+    "I think you can just say things like 'why not make me a santa clause'" to "'a close up image of a woman in light makeup in a santa clause costume"
+    with "a woman in light makeup" being the description of the image. Only output the stable diffusion prompt.
+    """
+    sys_prompt = prompt_for_GPTV if use_gptv else prompt_for_GPT
+    return sys_prompt
+    
 class Pipeline:
     class Info(BaseModel):
         name: str = "img2img"
@@ -145,8 +167,73 @@ class Pipeline:
             requires_pooled=[False, True],
         )
 
+    
+        
+    # call openai to generate a response using chatCompletion api
+    def call_openai(system_prompt, input_text, model="gpt-4-1106-preview"):
+        completion = client.chat.completions.create(
+            # Use GPT 3.5 as the LLM
+            temperature=0.7,
+            model=model,
+            # Pre-define conversation messages for the possible roles
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": input_text},
+            ],
+        )
+        return completion.choices[0].message.content
+    
+    def call_openai_gptv(system_prompt, input_text, image_base64, model='gpt-4-vision-preview'):
+        messages = [
+            {
+                "role": "system",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": system_prompt
+                    },
+                ],
+            },
+            {
+               "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": input_text,
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": f"data:image/png;base64,{image_base64}"},
+                    },
+                ],
+            },
+        ]
+        response = client.chat.completions.create(
+            model=model,
+            messages=messages,
+            max_tokens=300,
+        )
+        reply = response.choices[0].message.content
+        return reply
+
+    def alter_prompt(self, params):
+        import time
+        prompt = params.prompt
+        use_gptv = params.use_gptv if hasattr(params, 'use_gptv') else False
+        image_base64 = pil_to_base64(params.image)
+        sys_prompt = get_sys_promt(use_gptv)
+        print(f"useGPTV {use_gptv}")
+        start_time = time.time()
+        # user_input = "I think you can just say things like 'why not make me a santa clause'"
+        output = call_openai_gptv(sys_prompt, prompt, image_base64=image_base64) if use_gptv else call_openai(sys_prompt, user_input) 
+        print(f"duration for calling GPTV/{use_gptv}: {time.time() - start_time}")
+        params.prompt = output
+    
     def predict(self, params: "Pipeline.InputParams") -> Image.Image:
         generator = torch.manual_seed(params.seed)
+        print(params.image)
+        self.alter_prompt(params)
+
         prompt_embeds, pooled_prompt_embeds = self.pipe.compel_proc(
             [params.prompt, params.negative_prompt]
         )
@@ -154,7 +241,7 @@ class Pipeline:
         strength = params.strength
         if int(steps * strength) < 1:
             steps = math.ceil(1 / max(0.10, strength))
-        
+
         results = self.pipe(
             image=params.image,
             prompt_embeds=prompt_embeds[0:1],
